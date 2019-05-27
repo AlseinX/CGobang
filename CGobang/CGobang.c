6cc31b2a6feec4cc4c1cc05b91e6e6f8286c4f01
AlseinX <xyh951115@live.com>
2019-05-27 18:10:13 +0800

    Initial commit.

---
+++ CGobang/CGobang.c
@@ -0,0 +1,15 @@
+﻿// CGobang.cpp: 定义应用程序的入口点。
+//
+
+#include "CGobang.h"
+
+int main()
+{
+	initscr();
+	move(2, 2);
+	printw("Hello world!");
+	refresh();
+	getch();
+	endwin();
+	return 0;
+}
---
+++ CGobang/CGobang.h
@@ -0,0 +1,6 @@
+﻿#ifndef CGOBANG_H
+#define CGOBANG_H
+
+#include <ncursesw/ncurses.h>
+
+#endif
